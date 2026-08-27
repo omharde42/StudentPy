23139bd9a5cb658b235eeb06a5e05aa3f72fe718
Om Harde-dev <omharde300@gmail.com>
2026-08-27 14:01:18 +0530

    Create 05_perimeter_of_rectangle.py

---
+++ Class-09/Input-Output/05_perimeter_of_rectangle.py
@@ -0,0 +1,10 @@
+# StudentPy
+# Class 9 - Input and Output
+# Program: Perimeter of a Rectangle
+
+length = float(input("Enter the length: "))
+width = float(input("Enter the width: "))
+
+perimeter = 2 * (length + width)
+
+print("Perimeter of rectangle:", perimeter)
