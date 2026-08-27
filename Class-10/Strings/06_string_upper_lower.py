f3d527a609999c1ac7639ab69fbefd6f9b0066a6
Om Harde-dev <omharde300@gmail.com>
2026-08-27 21:41:25 +0530

    Create 06_string_upper_lower.py

---
+++ Class-10/Strings/06_string_upper_lower.py
@@ -0,0 +1,8 @@
+# StudentPy
+# Class 10 - Strings
+# Program: Convert String to Uppercase and Lowercase
+
+text = input("Enter a string: ")
+
+print("Uppercase:", text.upper())
+print("Lowercase:", text.lower())
