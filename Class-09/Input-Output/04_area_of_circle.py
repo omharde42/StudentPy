a73d9704436bbaa0fbdfeccbbd719a31c57e9d96
Om Harde-dev <omharde300@gmail.com>
2026-08-27 14:00:56 +0530

    Add program to calculate area of a circle

---
+++ Class-09/Input-Output/04_area_of_circle.py
@@ -0,0 +1,9 @@
+# StudentPy
+# Class 9 - Input and Output
+# Program: Area of a Circle
+
+radius = float(input("Enter the radius of the circle: "))
+
+area = 3.14159 * radius * radius
+
+print("Area of circle:", area)
