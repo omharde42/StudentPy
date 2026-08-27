2b5283da1db5275e0ed7c9fd2ce551318357d0a0
Om Harde-dev <omharde300@gmail.com>
2026-08-27 14:13:54 +0530

    Add pass or fail check based on student marks
    This program checks if a student has passed or failed based on their marks.

---
+++ Class-09/Conditions/06_pass_or_fail.py
@@ -0,0 +1,10 @@
+# StudentPy
+# Class 9 - Conditions
+# Program: Check Pass or Fail
+
+marks = float(input("Enter your marks: "))
+
+if marks >= 35:
+    print("Result: Pass")
+else:
+    print("Result: Fail")
