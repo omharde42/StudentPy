4804abb6a44e0ba7dd6cf8e3c169b36fd5dad526
Om Harde-dev <omharde300@gmail.com>
2026-08-27 20:49:31 +0530

    Create 10_break_and_continue.py

---
+++ Class-09/Loops/10_break_and_continue.py
@@ -0,0 +1,13 @@
+# StudentPy
+# Class 9 - Loops
+# Program: Using break and continue
+
+for number in range(1, 11):
+
+    if number == 5:
+        continue
+
+    if number == 9:
+        break
+
+    print(number)
