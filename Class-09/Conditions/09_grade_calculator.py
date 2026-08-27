0a18500f723adc62a1bd74b10b30096728481723
Om Harde-dev <omharde300@gmail.com>
2026-08-27 14:15:04 +0530

    Create 09_grade_calculator.py

---
+++ Class-09/Conditions/09_grade_calculator.py
@@ -0,0 +1,20 @@
+# StudentPy
+# Class 9 - Conditions
+# Program: Grade Calculator
+
+marks = float(input("Enter your marks: "))
+
+if marks >= 90:
+    print("Grade: A+")
+elif marks >= 80:
+    print("Grade: A")
+elif marks >= 70:
+    print("Grade: B")
+elif marks >= 60:
+    print("Grade: C")
+elif marks >= 50:
+    print("Grade: D")
+elif marks >= 35:
+    print("Grade: E")
+else:
+    print("Grade: F")
