fd7e18518d4e409e29809e34eee00e1e7d12a1a5
Om Harde-dev <omharde300@gmail.com>
2026-08-27 21:46:01 +0530

    Create 10_list_sum_and_average.py

---
+++ Class-10/Lists/10_list_sum_and_average.py
@@ -0,0 +1,12 @@
+# StudentPy
+# Class 10 - Lists
+# Program: Calculate Sum and Average of List Elements
+
+numbers = [10, 20, 30, 40, 50]
+
+total = sum(numbers)
+average = total / len(numbers)
+
+print("Numbers:", numbers)
+print("Sum:", total)
+print("Average:", average)
