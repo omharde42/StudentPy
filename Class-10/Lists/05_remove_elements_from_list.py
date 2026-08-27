5ecbed93014c14fe1d8e9687ac6b943e555b4809
Om Harde-dev <omharde300@gmail.com>
2026-08-27 21:44:42 +0530

    Create 05_remove_elements_from_list.py

---
+++ Class-10/Lists/05_remove_elements_from_list.py
@@ -0,0 +1,11 @@
+# StudentPy
+# Class 10 - Lists
+# Program: Remove Elements from a List
+
+fruits = ["Apple", "Banana", "Mango", "Orange"]
+
+fruits.remove("Banana")
+removed_fruit = fruits.pop()
+
+print("Removed fruit:", removed_fruit)
+print("Updated list:", fruits)
