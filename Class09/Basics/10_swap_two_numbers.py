dfbfd1c880d360cead2766f45bd5fad05152aaca
Om Harde-dev <omharde300@gmail.com>
2026-08-27 13:55:21 +0530

    Create 10_swap_two_numbers.py

---
+++ Class09/Basics/10_swap_two_numbers.py
@@ -0,0 +1,12 @@
+# StudentPy
+# Class 9 - Python Basics
+# Program: Swap Two Numbers
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+a, b = b, a
+
+print("After swapping:")
+print("First number:", a)
+print("Second number:", b)
