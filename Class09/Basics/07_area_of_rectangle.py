e955db5763e352ff95832c80c710e50c4ed73189
Om Harde-dev <omharde300@gmail.com>
2026-08-27 13:53:46 +0530

    Create 07_area_of_rectangle.py

---
+++ Class09/Basics/07_area_of_rectangle.py
@@ -0,0 +1,10 @@
+# StudentPy
+# Class 9 - Python Basics
+# Program: Area of a Rectangle
+
+length = float(input("Enter length: "))
+width = float(input("Enter width: "))
+
+area = length * width
+
+print("Area of rectangle:", area)
