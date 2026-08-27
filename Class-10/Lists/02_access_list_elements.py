af81c4a1b565e19cc130220c01a672879e904ca4
Om Harde-dev <omharde300@gmail.com>
2026-08-27 21:43:27 +0530

    Create 02_access_list_elements.py

---
+++ Class-10/Lists/02_access_list_elements.py
@@ -0,0 +1,9 @@
+# StudentPy
+# Class 10 - Lists
+# Program: Access List Elements
+
+fruits = ["Apple", "Banana", "Mango", "Orange"]
+
+print("First fruit:", fruits[0])
+print("Second fruit:", fruits[1])
+print("Last fruit:", fruits[-1])
