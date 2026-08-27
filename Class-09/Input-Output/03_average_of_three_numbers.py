642cf652fffe873c230a19f6c15a656d6a8300f4
Om Harde-dev <omharde300@gmail.com>
2026-08-27 14:00:33 +0530

    Create 03_average_of_three_numbers.py

---
+++ Class-09/Input-Output/03_average_of_three_numbers.py
@@ -0,0 +1,11 @@
+# StudentPy
+# Class 9 - Input and Output
+# Program: Average of Three Numbers
+
+first_number = float(input("Enter the first number: "))
+second_number = float(input("Enter the second number: "))
+third_number = float(input("Enter the third number: "))
+
+average = (first_number + second_number + third_number) / 3
+
+print("Average:", average)
