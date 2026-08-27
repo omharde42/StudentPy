c4ec13e15204d43fe4c8647824d29cbba77d4fcc
Om Harde-dev <omharde300@gmail.com>
2026-08-27 14:02:03 +0530

    Create 08_kilometers_to_meters.py

---
+++ Class-09/Input-Output/08_kilometers_to_meters.py
@@ -0,0 +1,9 @@
+# StudentPy
+# Class 9 - Input and Output
+# Program: Kilometers to Meters
+
+kilometers = float(input("Enter distance in kilometers: "))
+
+meters = kilometers * 1000
+
+print("Distance in meters:", meters)
