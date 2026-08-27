6059eb471284ea0f066e9e9649c1fef01b2e8d18
Om Harde-dev <omharde300@gmail.com>
2026-08-27 21:40:42 +0530

    Create 04_string_indexing.py

---
+++ Class-10/Strings/04_string_indexing.py
@@ -0,0 +1,11 @@
+# StudentPy
+# Class 10 - Strings
+# Program: String Indexing
+
+text = input("Enter a string: ")
+
+if len(text) > 0:
+    print("First character:", text[0])
+    print("Last character:", text[-1])
+else:
+    print("The string is empty.")
