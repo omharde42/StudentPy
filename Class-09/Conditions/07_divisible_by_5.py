7192b6d782838ab8ee5882b077fc0d87735e7f49
Om Harde-dev <omharde300@gmail.com>
2026-08-27 14:14:11 +0530

    Create 07_divisible_by_5.py

---
+++ Class-09/Conditions/07_divisible_by_5.py
@@ -0,0 +1,10 @@
+# StudentPy
+# Class 9 - Conditions
+# Program: Check Divisibility by 5
+
+number = int(input("Enter an integer: "))
+
+if number % 5 == 0:
+    print("The number is divisible by 5.")
+else:
+    print("The number is not divisible by 5.")
