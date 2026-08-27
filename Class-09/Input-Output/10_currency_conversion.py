74334189fdd55f4ffc5cc25b514403735072d257
Om Harde-dev <omharde300@gmail.com>
2026-08-27 14:04:02 +0530

    Add currency conversion program in Python

---
+++ Class-09/Input-Output/10_currency_conversion.py
@@ -0,0 +1,10 @@
+# StudentPy
+# Class 9 - Input and Output
+# Program: Currency Conversion
+
+rupees = float(input("Enter amount in Indian Rupees: "))
+conversion_rate = float(input("Enter conversion rate: "))
+
+converted_amount = rupees * conversion_rate
+
+print("Converted amount:", converted_amount)
