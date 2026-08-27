4463a301e6510cfaaad6592fdc21b5dac3db4de9
Om Harde-dev <omharde300@gmail.com>
2026-08-27 14:12:34 +0530

    Create 02_even_odd.py

---
+++ Class-09/Conditions/02_even_odd.py
@@ -0,0 +1,10 @@
+# StudentPy
+# Class 9 - Conditions
+# Program: Check Even or Odd
+
+number = int(input("Enter an integer: "))
+
+if number % 2 == 0:
+    print("The number is even.")
+else:
+    print("The number is odd.")
