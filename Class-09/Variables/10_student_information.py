672dbea169fbdc03d26e78fe5c5a33a507b6258b
Om Harde-dev <omharde300@gmail.com>
2026-08-27 20:45:33 +0530

    Create 10_student_information.py

---
+++ Class-09/Variables/10_student_information.py
@@ -0,0 +1,12 @@
+# StudentPy
+# Class 9 - Variables
+# Program: Student Information
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+grade = int(input("Enter your class: "))
+
+print("\n--- Student Information ---")
+print("Name:", name)
+print("Age:", age)
+print("Class:", grade)
