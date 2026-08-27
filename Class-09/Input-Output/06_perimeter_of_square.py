dd441a34eef2fc7df58100b7ea5ff3670388e802
Om Harde-dev <omharde300@gmail.com>
2026-08-27 14:01:34 +0530

    Create 06_perimeter_of_square.py

---
+++ Class-09/Input-Output/06_perimeter_of_square.py
@@ -0,0 +1,9 @@
+# StudentPy
+# Class 9 - Input and Output
+# Program: Perimeter of a Square
+
+side = float(input("Enter the side of the square: "))
+
+perimeter = 4 * side
+
+print("Perimeter of square:", perimeter)
