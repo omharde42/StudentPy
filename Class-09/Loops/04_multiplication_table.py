533f5d614868408163792e476416cf36fcbc6671
Om Harde-dev <omharde300@gmail.com>
2026-08-27 20:47:40 +0530

    Create 04_multiplication_table.py

---
+++ Class-09/Loops/04_multiplication_table.py
@@ -0,0 +1,8 @@
+# StudentPy
+# Class 9 - Loops
+# Program: Multiplication Table
+
+number = int(input("Enter a number: "))
+
+for multiplier in range(1, 11):
+    print(number, "x", multiplier, "=", number * multiplier)
