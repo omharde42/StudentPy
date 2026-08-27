d58319c52e4a565552bba1b2abe95516bc79de90
Om Harde-dev <omharde300@gmail.com>
2026-08-27 21:44:28 +0530

    Create 04_add_elements_to_list.py

---
+++ Class-10/Lists/04_add_elements_to_list.py
@@ -0,0 +1,10 @@
+# StudentPy
+# Class 10 - Lists
+# Program: Add Elements to a List
+
+fruits = ["Apple", "Banana"]
+
+fruits.append("Mango")
+fruits.insert(1, "Orange")
+
+print("Updated list:", fruits)
