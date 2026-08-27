04511c040e6f5e38f9505cbdfa92ec2b855613b7
Om Harde-dev <omharde300@gmail.com>
2026-08-27 13:51:24 +0530

    Create 03_data_types.py

---
+++ Class09/Basics/03_data_types.py
@@ -0,0 +1,13 @@
+# StudentPy
+# Class 9 - Python Basics
+# Program: Basic Python Data Types
+
+name = "Student"       # String
+age = 15               # Integer
+height = 5.5           # Float
+is_student = True      # Boolean
+
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Student:", is_student)
