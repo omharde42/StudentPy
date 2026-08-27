c440d7ff94e328f49233c7213d474f31abbafed6
Om Harde-dev <omharde300@gmail.com>
2026-08-27 14:00:10 +0530

    Create 02_sum_two_numbers.py

---
+++ Class-09/Input-Output/02_sum_two_numbers.py
@@ -0,0 +1,10 @@
+# StudentPy
+# Class 9 - Input and Output
+# Program: Sum of Two Numbers
+
+first_number = float(input("Enter the first number: "))
+second_number = float(input("Enter the second number: "))
+
+total = first_number + second_number
+
+print("Sum:", total)
