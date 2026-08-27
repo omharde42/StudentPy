795913fab38b0f3fb1d7c9d7677361052f553336
Om Harde-dev <omharde300@gmail.com>
2026-08-27 21:42:22 +0530

    Create 09_replace_string.py

---
+++ Class-10/Strings/09_replace_string.py
@@ -0,0 +1,11 @@
+# StudentPy
+# Class 10 - Strings
+# Program: Replace Text in a String
+
+text = input("Enter a string: ")
+old_text = input("Enter the text to replace: ")
+new_text = input("Enter the replacement text: ")
+
+updated_text = text.replace(old_text, new_text)
+
+print("Updated string:", updated_text)
