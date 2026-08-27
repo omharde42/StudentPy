72ce106a12c9df65444126c56c4f0b82a2cb74b9
Om Harde-dev <omharde300@gmail.com>
2026-08-27 14:13:36 +0530

    Create 05_eligible_to_vote.py

---
+++ Class-09/Conditions/05_eligible_to_vote.py
@@ -0,0 +1,10 @@
+# StudentPy
+# Class 9 - Conditions
+# Program: Check Voting Eligibility
+
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    print("You are eligible to vote.")
+else:
+    print("You are not eligible to vote.")
