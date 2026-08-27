9547c6e56bcb837e7e88559e01154d57cf0b7602
Om Harde-dev <omharde300@gmail.com>
2026-08-27 20:43:07 +0530

    Create 02_multiple_variables.py

---
+++ Class-09/Variables/02_multiple_variables.py
@@ -0,0 +1,11 @@
+# StudentPy
+# Class 9 - Variables
+# Program: Using Multiple Variables
+
+name = "Student"
+age = 15
+city = "Pune"
+
+print("Name:", name)
+print("Age:", age)
+print("City:", city)
