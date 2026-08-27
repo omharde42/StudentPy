0c91c64d1fbba3234819ee9197c68c8826b62cc4
Om Harde-dev <omharde300@gmail.com>
2026-08-27 21:41:04 +0530

    Create 05_string_slicing.py

---
+++ Class-10/Strings/05_string_slicing.py
@@ -0,0 +1,9 @@
+# StudentPy
+# Class 10 - Strings
+# Program: String Slicing
+
+text = input("Enter a string: ")
+
+print("First three characters:", text[:3])
+print("Last three characters:", text[-3:])
+print("String from index 1 to 4:", text[1:5])
