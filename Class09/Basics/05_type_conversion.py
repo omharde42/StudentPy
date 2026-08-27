f58159eb1e8fb971cc7d478cff7baf0083a02cbd
Om Harde-dev <omharde300@gmail.com>
2026-08-27 13:52:33 +0530

    Create 05_type_conversion.py

---
+++ Class09/Basics/05_type_conversion.py
@@ -0,0 +1,10 @@
+# StudentPy
+# Class 9 - Python Basics
+# Program: Type Conversion
+
+number = input("Enter a number: ")
+
+number = int(number)
+
+print("Number:", number)
+print("Double:", number * 2)
