e0bb482d4f33c1851744c66ea19d341584bb399f
Om Harde-dev <omharde300@gmail.com>
2026-08-27 14:02:21 +0530

    Create 09_minutes_to_seconds.py

---
+++ Class-09/Input-Output/09_minutes_to_seconds.py
@@ -0,0 +1,9 @@
+# StudentPy
+# Class 9 - Input and Output
+# Program: Minutes to Seconds
+
+minutes = float(input("Enter time in minutes: "))
+
+seconds = minutes * 60
+
+print("Time in seconds:", seconds)
