97c48eb33c48c4e7aea92222ad761197e6014eed
Om Harde-dev <omharde300@gmail.com>
2026-08-27 20:45:16 +0530

    Create 09_swap_variables.py

---
+++ Class-09/Variables/09_swap_variables.py
@@ -0,0 +1,16 @@
+# StudentPy
+# Class 9 - Variables
+# Program: Swapping Two Variables
+
+first_number = 10
+second_number = 20
+
+print("Before swapping:")
+print("First number:", first_number)
+print("Second number:", second_number)
+
+first_number, second_number = second_number, first_number
+
+print("\nAfter swapping:")
+print("First number:", first_number)
+print("Second number:", second_number)
