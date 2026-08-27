d0726a42b01d646c4fee0cffcd96ccd04bcea4e4
Om Harde-dev <omharde300@gmail.com>
2026-08-27 21:40:29 +0530

    Create 03_string_concatenation.py

---
+++ Class-10/Strings/03_string_concatenation.py
@@ -0,0 +1,10 @@
+# StudentPy
+# Class 10 - Strings
+# Program: String Concatenation
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+full_name = first_name + " " + last_name
+
+print("Full name:", full_name)
