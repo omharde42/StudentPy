883dc11903753e392684e28c5edb7443701e872b
Om Harde-dev <omharde300@gmail.com>
2026-08-31 12:21:48 +0530

    Create 07_count_tuple_element.py

---
+++ Class-10/Tuples/07_count_tuple_element.py
@@ -0,0 +1,11 @@
+# StudentPy
+# Class 10 - Tuples
+# Program: Count an Element in a Tuple
+
+numbers = (10, 20, 10, 30, 10, 40)
+
+number = int(input("Enter a number to count: "))
+
+count = numbers.count(number)
+
+print("Number of occurrences:", count)
