479e3f02dc5c4320abee889386afe5b170a4d3a2
Om Harde-dev <omharde300@gmail.com>
2026-08-31 12:22:22 +0530

    Create 09_tuple_unpacking.py

---
+++ Class-10/Tuples/09_tuple_unpacking.py
@@ -0,0 +1,11 @@
+# StudentPy
+# Class 10 - Tuples
+# Program: Tuple Unpacking
+
+student = ("Om", 17, "Class 10")
+
+name, age, grade = student
+
+print("Name:", name)
+print("Age:", age)
+print("Class:", grade)
