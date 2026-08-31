2b4e03fca4f762c3a9091ad2922cf313aa867ac4
Om Harde-dev <omharde300@gmail.com>
2026-08-31 12:24:11 +0530

    Add program to update a dictionary item

---
+++ Class-10/Dictionaries/04_update_dictionary_item.py
@@ -0,0 +1,14 @@
+# StudentPy
+# Class 10 - Dictionaries
+# Program: Update a Dictionary Item
+
+student = {
+    "name": "Student",
+    "age": 15,
+    "class": 9
+}
+
+student["class"] = 10
+
+print("Updated class:", student["class"])
+print("Updated dictionary:", student)
