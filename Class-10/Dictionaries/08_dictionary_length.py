4b3d0a594cf49988d408296a14e1eadc1e30f702
Om Harde-dev <omharde300@gmail.com>
2026-08-31 12:25:05 +0530

    Create 08_dictionary_length.py

---
+++ Class-10/Dictionaries/08_dictionary_length.py
@@ -0,0 +1,12 @@
+# StudentPy
+# Class 10 - Dictionaries
+# Program: Find Dictionary Length
+
+student = {
+    "name": "Student",
+    "age": 15,
+    "class": 10,
+    "city": "Pune"
+}
+
+print("Number of items:", len(student))
