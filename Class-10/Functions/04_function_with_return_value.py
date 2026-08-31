59aa14a8c423815bb0a481d03ceccca19ffd255b
Om Harde-dev <omharde300@gmail.com>
2026-08-31 12:27:16 +0530

    Create 04_function_with_return_value.py

---
+++ Class-10/Functions/04_function_with_return_value.py
@@ -0,0 +1,13 @@
+# StudentPy
+# Class 10 - Functions
+# Program: Function with a Return Value
+
+def square(number):
+    return number * number
+
+
+number = float(input("Enter a number: "))
+
+result = square(number)
+
+print("Square:", result)
