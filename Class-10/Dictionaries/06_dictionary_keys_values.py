55456a2908bd34884e288c1dcf8681367357d95e
Om Harde-dev <omharde300@gmail.com>
2026-08-31 12:24:38 +0530

    Create 06_dictionary_keys_values.py

---
+++ Class-10/Dictionaries/06_dictionary_keys_values.py
@@ -0,0 +1,13 @@
+# StudentPy
+# Class 10 - Dictionaries
+# Program: Display Dictionary Keys and Values
+
+student = {
+    "name": "Student",
+    "age": 15,
+    "class": 10
+}
+
+print("Keys:", student.keys())
+print("Values:", student.values())
+print("Items:", student.items())
