d7ef69690758e18dd1f5adcae290c930b9b01ed8
Om Harde-dev <omharde300@gmail.com>
2026-08-31 12:23:57 +0530

    Create 03_add_dictionary_item.py

---
+++ Class-10/Dictionaries/03_add_dictionary_item.py
@@ -0,0 +1,12 @@
+# StudentPy
+# Class 10 - Dictionaries
+# Program: Add an Item to a Dictionary
+
+student = {
+    "name": "Student",
+    "age": 15
+}
+
+student["city"] = "Pune"
+
+print("Updated dictionary:", student)
