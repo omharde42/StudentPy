232a30d7b763121bc6a377c59a8542691ea91ba8
Om Harde-dev <omharde300@gmail.com>
2026-08-31 12:29:18 +0530

    Add student_details function with keyword arguments

---
+++ Class-10/Functions/08_keyword_arguments.py
@@ -0,0 +1,15 @@
+# StudentPy
+# Class 10 - Functions
+# Program: Keyword Arguments
+
+def student_details(name, age, grade):
+    print("Name:", name)
+    print("Age:", age)
+    print("Class:", grade)
+
+
+student_details(
+    name="Student",
+    age=15,
+    grade=10
+)
