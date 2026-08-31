d3219c1dadf4fbcb28c56303624bb2a8930fd582
Om Harde-dev <omharde300@gmail.com>
2026-08-31 12:25:17 +0530

    Create 09_loop_through_dictionary.py

---
+++ Class-10/Dictionaries/09_loop_through_dictionary.py
@@ -0,0 +1,12 @@
+# StudentPy
+# Class 10 - Dictionaries
+# Program: Loop Through a Dictionary
+
+student = {
+    "name": "Student",
+    "age": 15,
+    "class": 10
+}
+
+for key, value in student.items():
+    print(key, ":", value)
