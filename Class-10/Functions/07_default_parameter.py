1324151413301f34d44e6618b9b03cd3f5274c0a
Om Harde-dev <omharde300@gmail.com>
2026-08-31 12:28:58 +0530

    Create 07_default_parameter.py

---
+++ Class-10/Functions/07_default_parameter.py
@@ -0,0 +1,13 @@
+# StudentPy
+# Class 10 - Functions
+# Program: Default Parameter
+
+def greet(name="Student"):
+    print("Hello,", name)
+
+
+greet()
+
+name = input("Enter your name: ")
+
+greet(name)
