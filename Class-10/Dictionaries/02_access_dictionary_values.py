43eb85b190b99f6066d17408eed6f12008589ee3
Om Harde-dev <omharde300@gmail.com>
2026-08-31 12:23:44 +0530

    Create 02_access_dictionary_values.py

---
+++ Class-10/Dictionaries/02_access_dictionary_values.py
@@ -0,0 +1,13 @@
+# StudentPy
+# Class 10 - Dictionaries
+# Program: Access Dictionary Values
+
+student = {
+    "name": "Student",
+    "age": 15,
+    "class": 10
+}
+
+print("Name:", student["name"])
+print("Age:", student["age"])
+print("Class:", student["class"])
