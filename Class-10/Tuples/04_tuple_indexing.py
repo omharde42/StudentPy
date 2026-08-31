8e813c56be603fccb0d20c2130df7f845d588a93
Om Harde-dev <omharde300@gmail.com>
2026-08-31 12:21:03 +0530

    Create 04_tuple_indexing.py

---
+++ Class-10/Tuples/04_tuple_indexing.py
@@ -0,0 +1,9 @@
+# StudentPy
+# Class 10 - Tuples
+# Program: Tuple Indexing
+
+colors = ("Red", "Green", "Blue", "Yellow", "Purple")
+
+print("Element at index 0:", colors[0])
+print("Element at index 2:", colors[2])
+print("Element at index -1:", colors[-1])
