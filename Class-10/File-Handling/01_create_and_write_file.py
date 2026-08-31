feaa705c41913dcbe1b5dd3f58c7f4ce2f617c75
Om Harde-dev <omharde300@gmail.com>
2026-08-31 12:35:58 +0530

    Create 01_create_and_write_file.py

---
+++ Class-10/File-Handling/01_create_and_write_file.py
@@ -0,0 +1,9 @@
+# StudentPy
+# Class 10 - File Handling
+# Program: Create and Write to a File
+
+with open("student.txt", "w") as file:
+    file.write("Welcome to StudentPy!\n")
+    file.write("Python is easy to learn.")
+
+print("Data written successfully.")
