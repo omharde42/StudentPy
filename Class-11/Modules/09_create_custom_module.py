c86d9534f140fea6ede8f70a7af38f893a4d5efb
Om Harde-dev <omharde300@gmail.com>
2026-09-01 22:19:09 +0530

    Create 09_create_custom_module.py

---
+++ Class-11/Modules/09_create_custom_module.py
@@ -0,0 +1,16 @@
+# StudentPy
+# Class 11 - Modules
+# Program: Create Functions for a Custom Module
+
+def add(a, b):
+    return a + b
+
+
+def multiply(a, b):
+    return a * b
+
+
+if __name__ == "__main__":
+    print("Custom module functions created.")
+    print("Add:", add(10, 5))
+    print("Multiply:", multiply(10, 5))
