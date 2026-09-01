7eb43c7a17336cfd104a5951e7151d00f00701f2
Om Harde-dev <omharde300@gmail.com>
2026-09-01 22:18:12 +0530

    Create 05_statistics_module.py

---
+++ Class-11/Modules/05_statistics_module.py
@@ -0,0 +1,11 @@
+# StudentPy
+# Class 11 - Modules
+# Program: Using the statistics Module
+
+import statistics
+
+marks = [75, 82, 91, 68, 88]
+
+print("Marks:", marks)
+print("Mean:", statistics.mean(marks))
+print("Median:", statistics.median(marks))
