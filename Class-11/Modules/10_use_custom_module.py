a76e8811b628dbf254d429646056b7b51838af07
Om Harde-dev <omharde300@gmail.com>
2026-09-01 22:19:43 +0530

    Create 10_use_custom_module.py

---
+++ Class-11/Modules/10_use_custom_module.py
@@ -0,0 +1,11 @@
+# StudentPy
+# Class 11 - Modules
+# Program: Import a Custom Module
+
+import student_math
+
+first_number = 10
+second_number = 5
+
+print("Sum:", student_math.add(first_number, second_number))
+print("Product:", student_math.multiply(first_number, second_number))
