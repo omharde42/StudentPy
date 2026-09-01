a48859a5f85376253f7f0d68405c6e0970850004
Om Harde-dev <omharde300@gmail.com>
2026-09-01 22:07:27 +0530

    Create 09_set_length.py

---
+++ Class-11/09_set_length.py
@@ -0,0 +1,7 @@
+# StudentPy
+# Class 11 - Sets
+# Program: Find the Length of a Set
+
+subjects = {"Physics", "Chemistry", "Mathematics", "Computer Science"}
+
+print("Number of subjects:", len(subjects))
