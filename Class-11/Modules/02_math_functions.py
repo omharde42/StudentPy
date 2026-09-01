07eb662d56c0afc029083fd058234ee404cbcb16
Om Harde-dev <omharde300@gmail.com>
2026-09-01 22:17:27 +0530

    Create 02_math_functions.py

---
+++ Class-11/Modules/02_math_functions.py
@@ -0,0 +1,11 @@
+# StudentPy
+# Class 11 - Modules
+# Program: Using Math Functions
+
+import math
+
+number = float(input("Enter a number: "))
+
+print("Ceiling:", math.ceil(number))
+print("Floor:", math.floor(number))
+print("Absolute value:", math.fabs(number))
