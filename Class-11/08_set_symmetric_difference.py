967b0af9b8088635495e84936f3cbf09322e1510
Om Harde-dev <omharde300@gmail.com>
2026-09-01 22:07:13 +0530

    Create 08_set_symmetric_difference.py

---
+++ Class-11/08_set_symmetric_difference.py
@@ -0,0 +1,10 @@
+# StudentPy
+# Class 11 - Sets
+# Program: Symmetric Difference
+
+set_a = {1, 2, 3, 4}
+set_b = {3, 4, 5, 6}
+
+result = set_a.symmetric_difference(set_b)
+
+print("Symmetric difference:", result)
