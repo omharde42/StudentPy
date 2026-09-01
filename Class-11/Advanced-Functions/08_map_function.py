6363a67b7d5d7a326c0b25ffe200c569bac1451f
Om Harde-dev <omharde300@gmail.com>
2026-09-01 22:15:56 +0530

    Create 08_map_function.py

---
+++ Class-11/Advanced-Functions/08_map_function.py
@@ -0,0 +1,10 @@
+# StudentPy
+# Class 11 - Advanced Functions
+# Program: Using map()
+
+numbers = [1, 2, 3, 4, 5]
+
+squares = list(map(lambda number: number * number, numbers))
+
+print("Original numbers:", numbers)
+print("Squares:", squares)
