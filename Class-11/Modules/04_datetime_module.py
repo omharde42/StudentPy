c3c8e948682a7298fe3157f5a7da5a2c8c76e93a
Om Harde-dev <omharde300@gmail.com>
2026-09-01 22:17:55 +0530

    Create 04_datetime_module.py

---
+++ Class-11/Modules/04_datetime_module.py
@@ -0,0 +1,11 @@
+# StudentPy
+# Class 11 - Modules
+# Program: Using the datetime Module
+
+from datetime import datetime
+
+current_time = datetime.now()
+
+print("Current date and time:", current_time)
+print("Date:", current_time.date())
+print("Time:", current_time.time())
