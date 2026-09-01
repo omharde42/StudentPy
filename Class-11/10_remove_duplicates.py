eaca041f216f4e17152150ee4d4f699854134070
Om Harde-dev <omharde300@gmail.com>
2026-09-01 22:07:44 +0530

    Create 10_remove_duplicates.py

---
+++ Class-11/10_remove_duplicates.py
@@ -0,0 +1,10 @@
+# StudentPy
+# Class 11 - Sets
+# Program: Remove Duplicates from a List
+
+numbers = [10, 20, 10, 30, 20, 40, 30]
+
+unique_numbers = set(numbers)
+
+print("Original list:", numbers)
+print("Without duplicates:", unique_numbers)
