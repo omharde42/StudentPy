c7ae3427cf6bbf150759eab9ec3cfae05c82e8cd
Om Harde-dev <omharde300@gmail.com>
2026-09-01 22:15:41 +0530

    Create 07_lambda_function.py

---
+++ Class-11/Advanced-Functions/07_lambda_function.py
@@ -0,0 +1,9 @@
+# StudentPy
+# Class 11 - Advanced Functions
+# Program: Lambda Function
+
+square = lambda number: number * number
+
+number = int(input("Enter a number: "))
+
+print("Square:", square(number))
