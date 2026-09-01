bcc9e2ab895006e652c18cac50a3f48b457b3e1a
Om Harde-dev <omharde300@gmail.com>
2026-09-01 22:06:10 +0530

    Add membership check program for a set

---
+++ Class-11/04_check_membership.py
@@ -0,0 +1,12 @@
+# StudentPy
+# Class 11 - Sets
+# Program: Check Membership in a Set
+
+numbers = {10, 20, 30, 40, 50}
+
+number = int(input("Enter a number: "))
+
+if number in numbers:
+    print("Number is present in the set.")
+else:
+    print("Number is not present in the set.")
